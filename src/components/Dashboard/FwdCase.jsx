f9483fb086b25ee38aef7f37277ed74415d916a8
satyamjha03 <151564330+satyamjha03@users.noreply.github.com>
2023-11-20 23:52:35 +0530

    Add files via upload

---
+++ src/components/Dashboard/FwdCase.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import './fwdCase.css'
+
+const FwdCase = () => {
+    return (
+        <>
+            <div className="fwdMainContainer">
+                <div className="fwdcaseitem">
+                    <div className="fwdtop">
+                        <p className='fwdcaseid'>Case Id </p>
+                        <p id='fwdcasename'> Case Name</p>
+
+                    </div>
+                    <span className="date">13/12/23</span>
+                    <span className="fwdupdate">Closed</span>
+                </div>
+                <div className="fwdcaseitem">
+                    <div className="fwdtop">
+                        <p className='fwdcaseid'>Case Id </p>
+                        <p id='fwdcasename'> Case Name</p>
+
+                    </div>
+                    <span className="date">13/12/23</span>
+                    <span className="fwdupdate">Closed</span>
+                </div>
+                <div className="fwdcaseitem">
+                    <div className="fwdtop">
+                        <p className='fwdcaseid'>Case Id </p>
+                        <p id='fwdcasename'> Case Name</p>
+
+                    </div>
+                    <span className="date">13/12/23</span>
+                    <span className="fwdupdate">Closed</span>
+                </div>
+                <div className="fwdcaseitem">
+                    <div className="fwdtop">
+                        <p className='fwdcaseid'>Case Id </p>
+                        <p id='fwdcasename'> Case Name</p>
+
+                    </div>
+                    <span className="date">13/12/23</span>
+                    <span className="fwdupdate">Closed</span>
+                </div>
+                <div className="fwdcaseitem">
+                    <div className="fwdtop">
+                        <p className='fwdcaseid'>Case Id </p>
+                        <p id='fwdcasename'> Case Name</p>
+
+                    </div>
+                    <span className="date">13/12/23</span>
+                    <span className="fwdupdate">Closed</span>
+                </div>
+                <div className="fwdcaseitem">
+                    <div className="fwdtop">
+                        <p className='fwdcaseid'>Case Id </p>
+                        <p id='fwdcasename'> Case Name</p>
+
+                    </div>
+                    <span className="date">13/12/23</span>
+                    <span className="fwdupdate">Closed</span>
+                </div>
+
+
+            </div>
+
+        </>
+    )
+}
+
+export default FwdCase
